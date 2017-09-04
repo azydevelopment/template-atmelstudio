b897a520b276f0e419f7fa806aa55969e8bea9cc
azydevelopment <29720335+azydevelopment@users.noreply.github.com>
2017-09-03 21:12:59 -0700

    Added USART logging capability

--- template_atmelstudio/template_samd21j18a/program.cpp
+++ template_atmelstudio/template_samd21j18a/program.cpp
@@ -1,8 +1,13 @@
 #include "program.h"
 
-#include <asf.h>
+#include <cstring>
 
-CProgram::CProgram() {
+/* PUBLIC */
+
+CProgram::CProgram() :
+	ILogger(),
+	m_usart({}),
+	m_enable_logger(false) {
 }
 
 CProgram::~CProgram() {
@@ -20,6 +25,8 @@ void CProgram::OnInit() {
 	/* Initialize the SAM system */
 	system_init();
 	
+	InitLogger();
+	
 	struct port_config pin_conf;
 	port_get_config_defaults(&pin_conf);
 
@@ -35,7 +42,9 @@ void CProgram::OnInit() {
 }
 
 void CProgram::OnUpdate() {
-	/* Replace with your application code */
+
+	Log("Hello world");
+	
 	while (1)
 	{
 		/* Is button pressed? */
@@ -45,6 +54,51 @@ void CProgram::OnUpdate() {
 			} else {
 			/* No, so turn LED off. */
 			port_pin_set_output_level(PIN_PB30, true);
+		}
+	}
+}
+
+// ILogger
+
+void CProgram::InitLogger() {
+	struct usart_config config;
+	usart_get_config_defaults(&config);
+	config.pinmux_pad0 = PINMUX_PA22C_SERCOM3_PAD0;
+	config.pinmux_pad1 = PINMUX_PA23C_SERCOM3_PAD1;
+	config.pinmux_pad2 = PINMUX_UNUSED;
+	config.pinmux_pad3 = PINMUX_UNUSED;
+	config.mux_setting = USART_RX_1_TX_0_XCK_1;
+	config.baudrate = 9600;
+
+	// wait until the usart has been initialized
+	while(usart_init(&m_usart, SERCOM3, &config) != STATUS_OK);
+	
+	usart_enable(&m_usart);
+	
+	SetEnabledLogger(true);
+}
+
+void CProgram::SetEnabledLogger(bool enabled) {
+	m_enable_logger = enabled;
+}
+
+bool CProgram::IsEnabledLogger() {
+	return m_enable_logger;
+}
+
+void CProgram::LogEol() {
+	if (m_enable_logger) {
+		usart_write_wait(&m_usart, '\n');
+	}
+}
+
+void CProgram::Log(const char text[], bool eol) {
+	// TODO HACK: This probably isn't safe or efficient
+	if (m_enable_logger) {
+		// TODO HACK: C-style cast
+		usart_write_buffer_wait(&m_usart, (const uint8_t*) text, strlen(text));
+		if(eol) {
+			LogEol();
 		}
 	}
 }
--- template_atmelstudio/template_samd21j18a/program.h
+++ template_atmelstudio/template_samd21j18a/program.h
@@ -1,13 +1,30 @@
 #pragma once
 
-class CProgram final
+#include "abdeveng_core_service_logger.h"
+
+#include <asf.h>
+
+class CProgram final :
+public ILogger
 {
-public:
+	public:
+
 	CProgram();
 	virtual ~CProgram();
 	void Main();
 
-private:
+	// ILogger
+	virtual void InitLogger();
+	virtual void SetEnabledLogger(bool);
+	virtual bool IsEnabledLogger();
+	virtual void LogEol();
+	virtual void Log(const char[], bool eol = true);
+
+	private:
+
+	struct usart_module m_usart;
+	bool m_enable_logger;
+
 	void OnInit();
 	void OnUpdate();
 };
